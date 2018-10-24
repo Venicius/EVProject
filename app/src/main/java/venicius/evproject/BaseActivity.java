4d1b6056bf773e098d6454f093aee4c8727a3b4e
Venicius <venicius.alves@gmail.com>
2018-10-24 16:02:16 -0300

    24.10

---
+++ app/src/main/java/venicius/evproject/BaseActivity.java
@@ -0,0 +1,35 @@
+package venicius.evproject;
+
+
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Autenticando");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+}
+
---
+++ app/src/main/java/venicius/evproject/MainActivity.java
@@ -0,0 +1,41 @@
+package venicius.evproject;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.getSupportActionBar().hide();
+
+        Button buttonPlay = (Button)findViewById(R.id.btnPlay);
+        final Intent intentPlay = new Intent(this, FullscreenActivity.class);
+
+        FloatingActionButton fabConfig = (FloatingActionButton) findViewById(R.id.fabConfig);
+        final Intent intentConfig = new Intent(this, LoginActivity.class);
+
+        fabConfig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentConfig);
+            }
+        });
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(intentPlay);
+            }
+        });
+
+
+    }
+}
